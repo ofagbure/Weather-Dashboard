10297d657ee668ce8ee3fb1c3142949bb39bf1e7
Femi Fagbure <femifagbure@gmail.com>
2020-01-18 13:30:25 -0500

    massive edits to html, js and css

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Weather-Dashboard
 As a frequent traveler, I wanted to create a weather dashboard for a few of my most frequented/ loved cities. This application will also let me search for cities I plan to visit.
+
+## Table of Contents 
+
+* [Description](#Description)
+* [Functionality](#Functionality)
+* [License](#License)
+* [Badges](#Badges)
+
+## Description 
+
+This is a basic weather dashboard app that shows weather conditions for a given city. It shows the current weather as well as a 5 day forecast.
+
+## Functionality
+
+The app works by taking input from the user (a desired city), and then providing weather information for the city. The app provides basic current weather conditions, as well as a 5 day forecast for that city from the day the request is submitted
+
+
+## License
+
+MIT License
+
+Copyright (c) [2020] [Olufemi Fagbure]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+## Badges
+
+![GitHub last commit](https://img.shields.io/github/last-commit/ofagbure/Portfolio)
+
+
--- script.js
+++ script.js
@@ -1,24 +1,279 @@
-var queryURL = "api.openweathermap.org/data/2.5/weather?q="
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
 var apikey = "&appid=f48bf43dc4ee03b95ab57a63f2fc0386"
-var request = queryURL+"atlanta,ga"+apikey
-var forecastURL = "api.openweathermap.org/data/2.5/forecast?q="
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="
+var city = "atlanta"
+var request = queryURL + city + apikey
+var forecastRequest = forecastURL + city + apikey
 
+// current weather
 $.ajax({
     url: request,
     method: "GET"
-  })
-    .then(function(response) {
-        $(".city").text("Atlanta")
-        $(".temp").text("Temperature: "+ response.main.temp)
-        $(".feelsLike").text("Feels Like: "+response.main.feels_like)
-        $(".humidity").text("Humidity: "+response.main.humidity)
-        $(".windSpeed").text("Wind Speed: "+response.wind.speed)
+})
+    .then(function (response) {
+        console.log(response)
+        $(".city").text("Atlanta, GA")
+        $(".temp").text("Temperature: " + response.main.temp + " C")
+        $(".feelsLike").text("Feels Like: " + response.main.feels_like)
+        $(".humidity").text("Humidity: " + response.main.humidity + "%")
+        $(".windSpeed").text("Wind Speed: " + response.wind.speed + " MPH")
+    })
+// day 1 code
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        console.log(response)
+        $("#day1").text(response.list[2].dt_txt)
+        var icon1 = $("<img>").text(response.list[2].weather[0].icon)
+        $("#icon1").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[2].main.temp + "C")
+        $("#icon1").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[2].main.humidity + "%")
+        $("#icon1").append(iconhumid)
+    })
+// day 2
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        $("#day2").text(response.list[10].dt_txt)
+        var icon1 = $("<img>").text(response.list[10].weather[0].icon)
+        $("#icon2").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[10].main.temp + "C")
+        $("#icon2").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[10].main.humidity + "%")
+        $("#icon2").append(iconhumid)
+    })
+// day 3
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        $("#day3").text(response.list[18].dt_txt)
+        var icon1 = $("<img>").text(response.list[18].weather[0].icon)
+        $("#icon3").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[18].main.temp + "C")
+        $("#icon3").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[18].main.humidity + "%")
+        $("#icon3").append(iconhumid)
+    })
+// day 4
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        $("#day4").text(response.list[26].dt_txt)
+        var icon1 = $("<img>").text(response.list[26].weather[0].icon)
+        $("#icon4").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[26].main.temp + "C")
+        $("#icon4").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[26].main.humidity + "%")
+        $("#icon4").append(iconhumid)
+    })
+// day 5
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        $("#day5").text(response.list[34].dt_txt)
+        var icon1 = $("<img>").text(response.list[34].weather[0].icon)
+        $("#icon5").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[34].main.temp + "C")
+        $("#icon5").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[34].main.humidity + "%")
+        $("#icon5").append(iconhumid)
     })
 
+//  BUTTON CODE 
+$("#thisCity").click(function() {
+    var city = $(this).text
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
+    var apikey = "&appid=f48bf43dc4ee03b95ab57a63f2fc0386"
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="
+    var request = queryURL + city + apikey
+    var forecastRequest = forecastURL + city + apikey
+
     $.ajax({
-        url:forecastURL,
+        url: request,
         method: "GET"
-      })
-        .then(function(response) {
-         
+    })
+        .then(function (response) {
+            console.log(response)
+            $(".city").text($(this).text)
+            $(".temp").text("Temperature: " + response.main.temp + " C")
+            $(".feelsLike").text("Feels Like: " + response.main.feels_like)
+            $(".humidity").text("Humidity: " + response.main.humidity + "%")
+            $(".windSpeed").text("Wind Speed: " + response.wind.speed + " MPH")
         })
+    
+    // day 1 code
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        console.log(response)
+        $("#day1").text(response.list[2].dt_txt)
+        var icon1 = $("<img>").text(response.list[2].weather[0].icon)
+        $("#icon1").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[2].main.temp + "C")
+        $("#icon1").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[2].main.humidity + "%")
+        $("#icon1").append(iconhumid)
+    })
+// day 2
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        $("#day2").text(response.list[10].dt_txt)
+        var icon1 = $("<img>").text(response.list[10].weather[0].icon)
+        $("#icon2").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[10].main.temp + "C")
+        $("#icon2").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[10].main.humidity + "%")
+        $("#icon2").append(iconhumid)
+    })
+// day 3
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        $("#day3").text(response.list[18].dt_txt)
+        var icon1 = $("<img>").text(response.list[18].weather[0].icon)
+        $("#icon3").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[18].main.temp + "C")
+        $("#icon3").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[18].main.humidity + "%")
+        $("#icon3").append(iconhumid)
+    })
+// day 4
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        $("#day4").text(response.list[26].dt_txt)
+        var icon1 = $("<img>").text(response.list[26].weather[0].icon)
+        $("#icon4").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[26].main.temp + "C")
+        $("#icon4").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[26].main.humidity + "%")
+        $("#icon4").append(iconhumid)
+    })
+// day 5
+$.ajax({
+    url: forecastRequest,
+    method: "GET"
+})
+    .then(function (response) {
+        $("#day5").text(response.list[34].dt_txt)
+        var icon1 = $("<img>").text(response.list[34].weather[0].icon)
+        $("#icon5").append(icon1)
+        var icontemp = $("<p>").text("Temp: " + response.list[34].main.temp + "C")
+        $("#icon5").append(icontemp)
+        var iconhumid = $("<p>").text("Humidity: " + response.list[34].main.humidity + "%")
+        $("#icon5").append(iconhumid)
+    })
+});
+
+// SEARCH CODE
+$("#searchButton").click(function() {
+    var city = $("userCity").val.trim()
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
+    var apikey = "&appid=f48bf43dc4ee03b95ab57a63f2fc0386"
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="
+    var request = queryURL + city + apikey
+    var forecastRequest = forecastURL + city + apikey
+
+    $.ajax({
+        url: request,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response)
+            $(".city").text($(this).text)
+            $(".temp").text("Temperature: " + response.main.temp + " C")
+            $(".feelsLike").text("Feels Like: " + response.main.feels_like)
+            $(".humidity").text("Humidity: " + response.main.humidity + "%")
+            $(".windSpeed").text("Wind Speed: " + response.wind.speed + " MPH")
+        })
+        $.ajax({
+            url: forecastRequest,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(response)
+                $("#day1").text(response.list[2].dt_txt)
+                var icon1 = $("<img>").text(response.list[2].weather[0].icon)
+                $("#icon1").append(icon1)
+                var icontemp = $("<p>").text("Temp: " + response.list[2].main.temp + "C")
+                $("#icon1").append(icontemp)
+                var iconhumid = $("<p>").text("Humidity: " + response.list[2].main.humidity + "%")
+                $("#icon1").append(iconhumid)
+            })
+        // day 2
+        $.ajax({
+            url: forecastRequest,
+            method: "GET"
+        })
+            .then(function (response) {
+                $("#day2").text(response.list[10].dt_txt)
+                var icon1 = $("<img>").text(response.list[10].weather[0].icon)
+                $("#icon2").append(icon1)
+                var icontemp = $("<p>").text("Temp: " + response.list[10].main.temp + "C")
+                $("#icon2").append(icontemp)
+                var iconhumid = $("<p>").text("Humidity: " + response.list[10].main.humidity + "%")
+                $("#icon2").append(iconhumid)
+            })
+        // day 3
+        $.ajax({
+            url: forecastRequest,
+            method: "GET"
+        })
+            .then(function (response) {
+                $("#day3").text(response.list[18].dt_txt)
+                var icon1 = $("<img>").text(response.list[18].weather[0].icon)
+                $("#icon3").append(icon1)
+                var icontemp = $("<p>").text("Temp: " + response.list[18].main.temp + "C")
+                $("#icon3").append(icontemp)
+                var iconhumid = $("<p>").text("Humidity: " + response.list[18].main.humidity + "%")
+                $("#icon3").append(iconhumid)
+            })
+        // day 4
+        $.ajax({
+            url: forecastRequest,
+            method: "GET"
+        })
+            .then(function (response) {
+                $("#day4").text(response.list[26].dt_txt)
+                var icon1 = $("<img>").text(response.list[26].weather[0].icon)
+                $("#icon4").append(icon1)
+                var icontemp = $("<p>").text("Temp: " + response.list[26].main.temp + "C")
+                $("#icon4").append(icontemp)
+                var iconhumid = $("<p>").text("Humidity: " + response.list[26].main.humidity + "%")
+                $("#icon4").append(iconhumid)
+            })
+        // day 5
+        $.ajax({
+            url: forecastRequest,
+            method: "GET"
+        })
+            .then(function (response) {
+                $("#day5").text(response.list[34].dt_txt)
+                var icon1 = $("<img>").text(response.list[34].weather[0].icon)
+                $("#icon5").append(icon1)
+                var icontemp = $("<p>").text("Temp: " + response.list[34].main.temp + "C")
+                $("#icon5").append(icontemp)
+                var iconhumid = $("<p>").text("Humidity: " + response.list[34].main.humidity + "%")
+                $("#icon5").append(iconhumid)
+            })
+        });
